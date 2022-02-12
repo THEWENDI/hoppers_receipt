e56a934b998664e06cae83c2db2982452c24193a
Wendi Li <issacbbb@hotmaill.com>
2022-02-11 19:32:44 -0800

    initial commit

---
+++ src/test/java/com/wendi/hoppers_receipt/HoppersReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendi.hoppers_receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
